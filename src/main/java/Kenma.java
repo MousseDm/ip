0316ca934445172747e0c37b63b745ae385e73c3
MousseDm <e1297764@u.nus.edu.sg>
2025-08-20 18:12:28 +0800

    Rename the chatbot, add greeting and exit

---
+++ src/main/java/Kenma.java
@@ -0,0 +1,20 @@
+public class Kenma {
+    public static void main(String[] args) {
+        String logo =
+                " _  __ ______ _   _ __  __       \n"
+                        + "| |/ /|  ____| \\ | |  \\/  |   /\\ \n"
+                        + "| ' / | |__  |  \\| | \\  / |  /  \\ \n"
+                        + "|  <  |  __| | . ` | |\\/| | / /\\ \\\n"
+                        + "| . \\ | |____| |\\  | |  | |/ ____ \\\n"
+                        + "|_|\\_\\|______|_| \\_|_|  |_/_/    \\_\\\n";
+
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Kenma");
+//        System.out.println(" What can I do for you?");
+        System.out.println(logo);
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
