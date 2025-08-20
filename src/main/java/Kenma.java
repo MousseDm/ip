dd9f154e0dae788d0813f614de91685c495d7c9e
MousseDm <e1297764@u.nus.edu.sg>
2025-08-20 18:18:38 +0800

    Add echo and exit on bye for Level-1

--- src/main/java/Kenma.java
+++ src/main/java/Kenma.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Kenma {
     public static void main(String[] args) {
         String logo =
@@ -8,13 +10,26 @@ public class Kenma {
                         + "| . \\ | |____| |\\  | |  | |/ ____ \\\n"
                         + "|_|\\_\\|______|_| \\_|_|  |_/_/    \\_\\\n";
 
-
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Kenma");
-//        System.out.println(" What can I do for you?");
         System.out.println(logo);
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(" Hello! I'm Kenma");
+        System.out.println(" What can I do for you?");
+
         System.out.println("____________________________________________________________");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break; // 退出循环，结束程序
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" " + input);
+                System.out.println("____________________________________________________________");
+            }
+        }
     }
 }
